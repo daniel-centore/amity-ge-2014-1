d771b13fff572629976ece7a7b26173719c6fbad
drdanielfc <drdanielfc@localhost>
2014-03-18 22:19:40 -0400

    szdfasdfasdf

--- src/tetris/ITLPAI.java
+++ src/tetris/ITLPAI.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * To change this template file, choose Tools | Templates and open the template
+ * in the editor.
  */
 
 package tetris;
@@ -10,49 +10,49 @@ import AIHelper.BoardRater;
 import AIHelper.FinalRater;
 
 /**
- *
+ * 
  * @author justinbehymer
  */
-public class ITLPAI implements AI 
-{
-    
-BoardRater boardRater = new FinalRater();
-    
-public Move bestMove(Board board, Piece piece, Piece nextPiece, int limitHeight) 
-{
-		double bestScore = 1e20;
-		int bestX = 0;
-		int bestY = 0;
-		Piece bestPiece = piece;
-		Piece current = piece;
+public class ITLPAI implements AI {
 
-		// loop through all the rotations
-		do {
-			final int yBound = limitHeight - current.getHeight() + 1;
-			final int xBound = board.getWidth() - current.getWidth() + 1;
+    BoardRater boardRater = new FinalRater();
 
-			// For current rotation, try all the possible columns
-			for (int x = 0; x < xBound; x++) {
-				int y = board.dropHeight(current, x);
-				// piece does not stick up too far
-				if ((y < yBound) && board.canPlace(current, x, y)) {
-					Board testBoard = new Board(board);
-					testBoard.place(current, x, y);
-					testBoard.clearRows();
+    @Override
+    public Move bestMove(final Board board, final Piece piece,
+            final Piece nextPiece, final int limitHeight) {
+        double bestScore = 1e20;
+        int bestX = 0;
+        int bestY = 0;
+        Piece bestPiece = piece;
+        Piece current = piece;
 
-					double score = boardRater.rateBoard(testBoard);
+        // loop through all the rotations
+        do {
+            final int yBound = limitHeight - current.getHeight() + 1;
+            final int xBound = board.getWidth() - current.getWidth() + 1;
 
-					if (score < bestScore) {
-						bestScore = score;
-						bestX = x;
-						bestY = y;
-						bestPiece = current;
-					}
-				}
-			}
+            // For current rotation, try all the possible columns
+            for (int x = 0; x < xBound; x++) {
+                final int y = board.dropHeight(current, x);
+                // piece does not stick up too far
+                if (y < yBound && board.canPlace(current, x, y)) {
+                    final Board testBoard = new Board(board);
+                    testBoard.place(current, x, y);
+                    testBoard.clearRows();
 
-			current = current.nextRotation();
-		} while (current != piece);
+                    final double score = this.boardRater.rateBoard(testBoard);
+
+                    if (score < bestScore) {
+                        bestScore = score;
+                        bestX = x;
+                        bestY = y;
+                        bestPiece = current;
+                    }
+                }
+            }
+
+            current = current.nextRotation();
+        } while (current != piece);
 
 		Move move = new Move();
 		move.x = bestX;
@@ -63,8 +63,17 @@ public Move bestMove(Board board, Piece piece, Piece nextPiece, int limitHeight)
 }
 
     @Override
-    public void setRater(BoardRater r) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void setRater(final BoardRater r) {
+        throw new UnsupportedOperationException("Not supported yet."); // To
+                                                                       // change
+                                                                       // body
+                                                                       // of
+                                                                       // generated
+                                                                       // methods,
+                                                                       // choose
+                                                                       // Tools
+                                                                       // |
+                                                                       // Templates.
     }
-    
+
 }
